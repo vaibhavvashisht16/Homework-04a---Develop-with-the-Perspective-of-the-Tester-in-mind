63effb1db2c5beadd6b73c8fd0a7b03c0b61324a
vaibhavvashisht16 <90115721+vaibhavvashisht16@users.noreply.github.com>
2021-10-09 13:07:27 -0400

    Update GitHubTest.py

--- GitHubTest.py
+++ GitHubTest.py
@@ -1,20 +1,45 @@
-# By Vaibhav
 import unittest
-from GitHubApi import getrepo
+from unittest.mock import patch
+import GitHubApi
 
-class TestGitHub(unittest.TestCase):
 
-    def testGithub1(self):
-        self.assertEqual(getrepo('aljhdfjabfbfb'), False)
+class TestGetHub(unittest.TestCase):
 
-    def testGithub2(self):
-        self.assertNotEqual(getrepo('aljhdfjabfbfb'), True)
+    @patch("GitHubApi.getrepo")
+    def testConnection1(self, mock_connect):
+        mock_connect.return_value = [200, {"helloworld": "2",
+                                           "Assignment-2": "1",
+                                           "vaibhavvashisht16": "12",
+                                           "Triangle567": "8",
+                                           "CS546": "14",
+                                           "Homework-04a---Develop-with-the-Perspective-of-the-Tester-in-mind": "10",
+                                           "HW-05---Static-Code-Analysis": "4"}]
+        self.assertTrue(GitHubApi.getrepo("vaibhavvashisht16")[0])
 
-    def testGithub3(self):
-        self.assertEqual(getrepo('GautamP2393'), False)
+    @patch("GitHubApi.getrepo")
+    def test_repos1(self, mock_connect):
+        mock_connect.return_value = [200, {"helloworld": "2",
+                                           "Assignment-2": "1",
+                                           "vaibhavvashisht16": "12",
+                                           "Triangle567": "8",
+                                           "CS546": "14",
+                                           "Homework-04a---Develop-with-the-Perspective-of-the-Tester-in-mind": "10",
+                                           "HW-05---Static-Code-Analysis": "4"}]
+        self.assertIs(mock_connect()[1], GitHubApi.getrepo("vaibhavvashisht16")[1])
 
-    def testGithub4(self):
-        self.assertNotEqual(getrepo('GautamP2393'), True)
+    @patch("GitHubApi.getrepo")
+    def testConnection2(self, mock_connect):
+        mock_connect.return_value = [200, {""}]
+        self.assertTrue(GitHubApi.getrepo("GautamP2393")[0])
 
-    def testGithub5(self):
-        self.assertEqual(getrepo('vaibhavvashisht16'), True)
+    @patch("GitHubApi.getrepo")
+    def test_repos2(self, mock_connect):
+        mock_connect.return_value = [200, {""}]
+        self.assertIs(mock_connect()[1], GitHubApi.getrepo("GautamP2393")[1])
+
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestGetHub)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
